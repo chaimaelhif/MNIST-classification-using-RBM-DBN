2819e0b06887e67be6a254311e2bcc709905ce46
chaimaelhif <chaima.elhif@hotmail.fr>
2023-03-24 11:57:33 +0100

    correction rbm

--- principal_RBM_alpha.py
+++ principal_RBM_alpha.py
@@ -1,8 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-global n, p, x_im, y_im
-
 
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
@@ -13,25 +11,27 @@ def logit(z):
 
 
 class RBM:
-    def __init__(self, num_visible=p, num_hidden=10, mu=0, sigma=0.1):
+    def __init__(self, num_visible, num_hidden=10, mu=0, sigma=0.1):
         self.a = np.zeros(num_visible)
         self.b = np.zeros(num_hidden)
         self.W = np.random.normal(mu, sigma, size=(num_visible, num_hidden))
 
     def entree_sortie_rbm(self, v):
-        return sigmoid(self.b + sum(np.dot(v, self.W)))
+        return sigmoid(self.b + np.dot(v, self.W))
 
     def sortie_entree_rbm(self, h):
-        return sigmoid(self.a + sum(np.dot(h, self.W.T)))
+        return sigmoid(self.a + np.dot(h, self.W.T))
 
-    def train_rbm(self, data, epochs=100, learning_rate=0.1, batch_size=100):
+    def train_rbm(self, data, epochs=100, learning_rate=0.1, batch_size=100, early_stopping=5):
+        keep_track = 0
+        mse = 100
         for epoch in range(epochs):
             data_copy = data.copy()
             np.random.shuffle(data_copy)
 
             for batch in range(0, data.shape[0], batch_size):
                 data_batch = data_copy[
-                    batch : min(batch + batch_size, data.shape[0]), :
+                    batch: min(batch + batch_size, data.shape[0]), :
                 ]
 
                 v0 = data_batch
@@ -51,11 +51,19 @@ class RBM:
 
             h_epoch = np.random.binomial(1, self.entree_sortie_rbm(data))
             data_rec = np.random.binomial(1, self.sortie_entree_rbm(h_epoch))
-            mse = np.sum((data_rec - data) ** 2) / data.shape[0]
-            print(f"Epoch {epoch+1}/{epochs} - Error: {mse:.4f}")
+            previous_mse = mse
+            mse = np.sum((data_rec - data) ** 2) / data.size
+
+            if keep_track < early_stopping and mse == previous_mse:
+                keep_track += 1
+            elif keep_track == early_stopping:
+                break
+            print(f"Epoch {epoch+1}/{epochs} - Error: {mse:.3f}")
+
         return self
 
-    def generate_image_rbm(self, nb_data=1, nb_gibbs=100):
+    def generate_image_rbm(self, x_im, y_im, nb_data=1, nb_gibbs=100, plot=False):
+        p = self.W.shape[0]
         images = np.zeros((nb_data, p))
         for data in range(nb_data):
             v = np.random.binomial(1, 0.5 * np.ones(p))
@@ -65,11 +73,12 @@ class RBM:
 
             images[data, :] = v
 
-        # Reshape and Plot the generated images
-        fig, axes = plt.subplots(1, nb_data, figsize=(10, 2))
-        images = images.reshape((nb_data, x_im, y_im))
-        for i in range(nb_data):
-            axes[i].imshow(images[i], cmap="gray")
-            axes[i].axis("off")
-        plt.show()
+        if plot:
+            # Reshape and Plot the generated images
+            fig, axes = plt.subplots(1, nb_data, figsize=(10, 2))
+            images = images.reshape((nb_data, x_im, y_im))
+            for i in range(nb_data):
+                axes[i].imshow(images[i], cmap="gray")
+                axes[i].axis("off")
+            plt.show()
         return images
