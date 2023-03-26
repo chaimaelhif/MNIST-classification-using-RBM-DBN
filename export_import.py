8f6000764316c07cd6af2630d1d5931a935d3eef
chaimaelhif <chaima.elhif@hotmail.fr>
2023-03-26 13:37:26 +0200

    add export_import file

---
+++ export_import.py
@@ -0,0 +1,43 @@
+import pickle
+
+import numpy as np
+import scipy as sp
+
+from principal_DNN_MNIST import DNN
+
+
+def lire_alpha_digit(filename: str, indices=None):
+    mat = sp.io.loadmat(filename, simplify_cells=True)
+    bad = mat["dat"][indices, :]
+    images = np.zeros((bad.size, bad[0, 0].size))
+    im = 0  # image index
+    for i in range(bad.shape[0]):
+        for j in range(bad.shape[1]):
+            images[im, :] = bad[i, j].flatten()
+            im += 1
+
+    return images
+
+
+def lire_mnist(filename: str, indices: np.ndarray, data_type: str):
+    mnist_all = sp.io.loadmat(filename, simplify_cells=True)
+    key = data_type + "0"
+    data_mnist = (mnist_all[key] > 127).astype(int)
+    label = np.zeros(mnist_all[key].shape[0])
+    for i in indices[1:]:
+        key = data_type + str(i)
+        data_mnist = np.vstack([data_mnist, (mnist_all[key] > 127).astype(int)])
+        y = i * np.ones(mnist_all[key].shape[0])
+        label = np.concatenate([label, y], axis=0)
+    return data_mnist, label
+
+
+def import_model(filename: str) -> DNN:
+    with open(filename, "rb") as file:
+        model = pickle.load(file)
+    return model
+
+
+def save_model(filename: str, model: DNN):
+    with open(filename, "wb") as file:
+        pickle.dump(model, file)
