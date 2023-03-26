1a0e9f5f3ca989d7e7032e45b446ab9b942ae101
chaimaelhif <chaima.elhif@hotmail.fr>
2023-03-26 03:09:41 +0200

    update readme file

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # MNIST-classification-using-RBM-DBN
-Training a non-supervised model (Deep Belief Network(using RBM)) to Binary AlphaDigits dataset and evaluating the non-supervised algorithm on the MNIST database.
+Training a non-supervised model (Deep Belief Network(using RBMs)) to Binary AlphaDigits dataset and
+evaluating the non-supervised algorithm on the MNIST database with supervised learning.
+
+To reproduce the scores and plots in the report you need to run the main.py file. 
+
+1. Install the required librairies using "requirements.txt"
+2. Run the main file:
+   1. Use **"--action action"** (action in ["RBM", "DBN", "DNN5.1", "DNN5.2.1", "DNN5.2.2"]) 
+   to specify which questions you want to check 
+   2. Use **"--arg1 value"** (value in ["train", "test"])
+       - "train" to train a model and save it
+       - "test" to import a model and test it
+   3. Use **"--arg2 value"** (value in [True, False]) to specify if you want to pretrain the model or not
+   4. Use **"--arg3 value"** (value in ["nb_layers", "nb_neurons", "train_size"]) to specify which analysis to make:
+   compare the model with and without pretraining for different set of parameters
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import argparse
 import pickle
+
 import numpy as np
 import scipy as sp
+from sklearn.utils import shuffle
 
-from principal_RBM_alpha import RBM
+from plots import plot_loss
 from principal_DBN_alpha import DBN
 from principal_DNN_MNIST import DNN
+from principal_RBM_alpha import RBM
 
 X_MNIST = 28
 Y_MNIST = 28
@@ -13,15 +17,15 @@ X_BAD = 20
 Y_BAD = 16
 
 
-def lire_alpha_digit(filename: str, indices: np.ndarray = None):
+def lire_alpha_digit(filename: str, indices=None):
     mat = sp.io.loadmat(filename, simplify_cells=True)
     bad = mat["dat"][indices, :]
     images = np.zeros((bad.size, bad[0, 0].size))
-    k = 0  # image index
+    im = 0  # image index
     for i in range(bad.shape[0]):
         for j in range(bad.shape[1]):
-            images[k, :] = bad[i, j].flatten()
-            k += 1
+            images[im, :] = bad[i, j].flatten()
+            im += 1
 
     return images
 
@@ -40,75 +44,112 @@ def lire_mnist(filename: str, indices: np.ndarray, data_type: str):
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Description of the program')
+    parser.add_argument('--action',
+                        choices=["RBM", "DBN", "DNN5.1", "DNN5.2.1", "DNN5.2.2"],
+                        help='Generate images using RBM')
+    parser.add_argument('--arg1', choices=["train", "test"], help='To train or test model')
+    parser.add_argument('--arg2', help='Boolean to specify pretraining')
+    parser.add_argument('--arg3',
+                        choices=["nb_layers", "nb_neurons", "train_size"],
+                        help='To specify the type of analysis to make')
+    args = parser.parse_args()
+
     # Global Variables
     output_dim = 10
     epochs_rbm = 100
     epochs_dnn = 200
-    learning_rate = 0.1
-    batch_size = 100
+    learning_rate = 0.08
+    batch_size = 128
     nb_gibbs = 500
     digits = np.arange(0, output_dim)
 
-    # Binary Alpha Digits (BAD)
+    # Import dataset: Binary Alpha Digits (BAD)
     path = "data/binaryalphadigs.mat"
-    data = lire_alpha_digit(path, digits)
+    data = lire_alpha_digit(path, [5])
     n_bad, p_bad = data.shape
 
-    # MNIST
+    # Import dataset: MNIST
     path = "data/mnist_all.mat"
     mnist_train, label_train = lire_mnist(path, digits, "train")
     mnist_test, label_test = lire_mnist(path, digits, "test")
     n_mnist, p_mnist = mnist_train.shape
 
-    args = "DNN"
-
-    if args == "RBM":
+    if args.action == "RBM":
         # Test RBM
         q = 200  # number of hidden values
-        config = (p_bad, 100, 100, 50)
         rbm = RBM(p_bad, q)
         rbm.train_rbm(data, epochs_rbm, learning_rate, batch_size)
         rbm.generate_image_rbm(X_BAD, Y_BAD, 4, nb_gibbs, True)
 
-    elif args == "DBN":
+    elif args.action == "DBN":
         # Test DBN
-        config = (p_bad, 100, 100, 50)
+        config = (p_bad, 200, 200)
         dbn = DBN(config)
         dbn.train_dbn(data, epochs_dnn, learning_rate, batch_size)
         dbn.generate_image_dbn(X_BAD, Y_BAD, 4, nb_gibbs, True)
 
-    elif args == "DNN":
+    elif args.action == "DNN5.1":
         # Test DNN (5.1)
-        config = (p_mnist, 100, 100, 50)
-        dnn = DNN(config)
+        config = (p_mnist, 200, 200)
 
-        dnn.pretrain_dnn(mnist_train, epochs_rbm, learning_rate, batch_size)
-        dnn.backward_propagation(
-            mnist_train, label_train, epochs_dnn, learning_rate, batch_size
-        )
+        if args.arg1 == "train":
+            dnn = DNN(config)
+            if args.arg2:  # Train model (with pretraining)
+                dnn.pretrain_dnn(mnist_train, epochs_rbm, learning_rate, batch_size)
+
+            dnn.backward_propagation(
+                mnist_train, label_train, epochs_dnn, learning_rate, batch_size
+            )
+
+            # Save model
+            with open(f"dnn_testPretrained{dnn.pretrained}.pkl", "wb") as f:
+                pickle.dump(dnn, f)
+
+        elif args.arg1 == "test":
+            # Import model
+            with open("dnn_test.pkl", "rb") as f:
+                dnn = pickle.load(f)
+
+        # Test model
         dnn.test_dnn(mnist_test, label_test)
-        idx = np.where(label_test == 1)[0]
 
-        # plot probabilities of the first class
-        dnn.plot_proba(mnist_test.iloc[idx, :])
+        # Plot probabilities of the class k
+        k = 2  # Choose a class and plot the probability
+        idx = np.where(label_test == k)[0]
+        dnn.plot_proba(mnist_test[idx])
 
-        # save model
-        with open("dnn_test.pkl", "wb") as f:
-            pickle.dump(dnn, f)
-
+    elif args.action == "DNN5.2.1":
         # Test 2 DNN := pretrained VS not pretrained (5.2)
-        # 1
-        dnn1 = DNN(config)  # to pretrain
-        dnn2 = DNN(config)
+        config = (p_mnist, 200, 200)
 
-        # 2
-        dnn1.pretrain_dnn(mnist_train, epochs_rbm, learning_rate, batch_size)
+        if args.arg1 == "train":
+            # 1
+            dnn1 = DNN(config)  # to pretrain
+            dnn2 = DNN(config)
 
-        # 3
-        dnn1.backward_propagation(mnist_train, label_train, epochs_dnn, learning_rate, batch_size)
+            # 2
+            dnn1.pretrain_dnn(mnist_train, epochs_rbm, learning_rate, batch_size)
 
-        # 4
-        dnn2.backward_propagation(mnist_train, label_train, epochs_dnn, learning_rate, batch_size)
+            # 3
+            dnn1.backward_propagation(mnist_train, label_train, epochs_dnn, learning_rate, batch_size)
+
+            # 4
+            dnn2.backward_propagation(mnist_train, label_train, epochs_dnn, learning_rate, batch_size)
+
+            # Save both models
+            with open(f"dnn_testPretrained{dnn1.pretrained}.pkl", "wb") as f:
+                pickle.dump(dnn1, f)  # pretrained
+            with open(f"dnn_testPretrained{dnn2.pretrained}.pkl", "wb") as f:
+                pickle.dump(dnn2, f)
+
+        elif args.arg1 == "test":
+            # Import models
+            with open("dnn_testPretrainedTrue.pkl", "rb") as f:
+                dnn1 = pickle.load(f)
+
+            with open("dnn_testPretrainedFalse.pkl", "rb") as f:
+                dnn2 = pickle.load(f)
 
         # 5
         print("DNN with pretraining:")
@@ -124,3 +165,128 @@ if __name__ == "__main__":
 
         print("For Test dataset:")
         dnn2.test_dnn(mnist_test, label_test)
+
+    # 6
+    elif args.action == "DNN5.2.2":
+        if args.arg3 == "nb_layers":  # Test with different number layers
+            error_rate1, error_rate2 = [], []
+            nb_layers = [2, 3, 5, 7]
+            for layer in nb_layers:
+                # Define the config for each iteration
+                config = [p_mnist]
+                config.extend([200 for x in range(layer)])
+
+                if args.arg1 == "train":
+                    dnn1 = DNN(config)  # to pretrain
+                    dnn2 = DNN(config)
+
+                    # Pretrain model then train it
+                    dnn1.pretrain_dnn(mnist_train, epochs_rbm, learning_rate, batch_size)
+                    dnn1.backward_propagation(mnist_train, label_train, epochs_dnn, learning_rate, batch_size)
+
+                    # Train model directly
+                    dnn2.backward_propagation(mnist_train, label_train, epochs_dnn, learning_rate, batch_size)
+
+                    # Save both models
+                    with open(f"dnn_testPretrained{dnn1.pretrained}_NbLayers{str(layer)}.pkl", "wb") as f:
+                        pickle.dump(dnn1, f)  # pretrained
+                    with open(f"dnn_testPretrained{dnn2.pretrained}_NbLayers{str(layer)}.pkl", "wb") as f:
+                        pickle.dump(dnn2, f)
+
+                elif args.arg1 == "test":
+                    # Import models
+                    with open(f"dnn_testPretrainedTrue_NbLayers{str(layer)}.pkl", "rb") as f:
+                        dnn1 = pickle.load(f)
+                    with open(f"dnn_testPretrainedFalse_NbLayers{str(layer)}.pkl", "rb") as f:
+                        dnn2 = pickle.load(f)
+
+                # Compute error rate for both models
+                error1 = dnn1.test_dnn(mnist_test, label_test)
+                error2 = dnn2.test_dnn(mnist_test, label_test)
+
+                error_rate1.append(error1)
+                error_rate2.append(error2)
+
+            plot_loss(error_rate1, error_rate2, nb_layers, "Number of layers")
+
+            if args.arg3 == "nb_neurons":  # Test with different number neurons
+                error_rate1, error_rate2 = [], []
+                nb_neurons = [100, 200, 300, 700]
+                for neurons in nb_neurons:
+                    # Define the config for each iteration
+                    config = [p_mnist]
+                    config.extend([neurons for x in range(3)])
+
+                    if args.arg1 == "train":
+                        dnn1 = DNN(config)  # to pretrain
+                        dnn2 = DNN(config)
+
+                        # Pretrain model then train it
+                        dnn1.pretrain_dnn(mnist_train, epochs_rbm, learning_rate, batch_size)
+                        dnn1.backward_propagation(mnist_train, label_train, epochs_dnn, learning_rate, batch_size)
+
+                        # Train model directly
+                        dnn2.backward_propagation(mnist_train, label_train, epochs_dnn, learning_rate, batch_size)
+
+                        # Save both models
+                        with open(f"dnn_testPretrained{dnn1.pretrained}_NbNeurons{str(neurons)}.pkl", "wb") as f:
+                            pickle.dump(dnn1, f)  # pretrained
+                        with open(f"dnn_testPretrained{dnn2.pretrained}_NbNeuron{str(neurons)}.pkl", "wb") as f:
+                            pickle.dump(dnn2, f)
+                    elif args.arg1 == "test":
+                        # Import models
+                        with open(f"dnn_testPretrainedTrue_NbNeurons{str(neurons)}.pkl", "rb") as f:
+                            dnn1 = pickle.load(f)
+                        with open(f"dnn_testPretrainedFalse_NbNeurons{str(neurons)}.pkl", "rb") as f:
+                            dnn2 = pickle.load(f)
+
+                    # Compute error rate for both models
+                    error1 = dnn1.test_dnn(mnist_test, label_test)
+                    error2 = dnn2.test_dnn(mnist_test, label_test)
+
+                    error_rate1.append(error1)
+                    error_rate2.append(error2)
+
+                plot_loss(error_rate1, error_rate2, nb_neurons, "Number of neurons")
+
+        if args.arg3 == "nb_layers":  # Test with different size of training data
+            error_rate1, error_rate2 = [], []
+            train_size = [1000, 3000, 7000, 10000, 30000, 60000]
+            for size in train_size:
+                config = (p_mnist, 100, 100, 50)
+
+                if args.arg1 == "train":
+                    dnn1 = DNN(config)  # to pretrain
+                    dnn2 = DNN(config)
+
+                    # Select the training data for each iteration
+                    data_shuffled, labels_shuffled = shuffle(mnist_train, label_train)
+                    data_sampled, labels_sampled = data_shuffled[:size], labels_shuffled[:size]
+
+                    # Pretrain model then train it
+                    dnn1.pretrain_dnn(data_sampled, epochs_rbm, learning_rate, batch_size)
+                    dnn1.backward_propagation(data_sampled, labels_sampled, epochs_dnn, learning_rate, batch_size)
+
+                    # Train model directly
+                    dnn2.backward_propagation(data_sampled, labels_sampled, epochs_dnn, learning_rate, batch_size)
+
+                    # Save both models
+                    with open(f"dnn_testPretrained{dnn1.pretrained}_TrainSize{str(size)}.pkl", "wb") as f:
+                        pickle.dump(dnn1, f)  # pretrained
+                    with open(f"dnn_testPretrained{dnn2.pretrained}_TrainSize{str(size)}.pkl", "wb") as f:
+                        pickle.dump(dnn2, f)
+                elif args.arg1 == "test":
+                    # Import models
+                    with open(f"dnn_testPretrainedTrue_TrainSize{str(size)}.pkl", "rb") as f:
+                        dnn1 = pickle.load(f)
+                    with open(f"dnn_testPretrainedFalse_TrainSize{str(size)}.pkl", "rb") as f:
+                        dnn2 = pickle.load(f)
+
+                # Compute error rate for both models
+                error1 = dnn1.test_dnn(mnist_test, label_test)
+                error2 = dnn2.test_dnn(mnist_test, label_test)
+
+                error_rate1.append(error1)
+                error_rate2.append(error2)
+
+            plot_loss(error_rate1, error_rate2, train_size, "Sample size")
